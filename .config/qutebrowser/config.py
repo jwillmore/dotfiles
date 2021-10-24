0b13e3bc36bac399fe8dbe135708decd6866141f
jwillmore <sbc@linuxcrusader.xyz>
2021-10-24 20:50:56 -0300

    Updated qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,9 @@
-config.load_autoconfig()
+#config.load_autoconfig()
 config.source('gruvbox.py')
 
 config.bind(';D', 'hint images download')
 
 c.url.start_pages = ["~/.config/qutebrowser/page.html"]
 c.url.default_page = "~/.config/qutebrowser/page.html"
+
+config.set("colors.webpage.darkmode.enabled", True)

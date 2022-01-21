e8154c75e31463bccdfc1679c23bd6b10a5d6449
jwillmore <sbc@linuxcrusader.xyz>
2022-01-21 16:14:18 -0300

    Fixed minor statusbar issues

--- .config/i3status-rust/wifi.sh
+++ .config/i3status-rust/wifi.sh
@@ -3,7 +3,7 @@
 wifi=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2)
 
 if [[ -z "$wifi" ]]; then
-  wifi=""
+  wifi=" Arch Linux"
 else
   wifi=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2 | sed 's/yes:/  /g')
 fi

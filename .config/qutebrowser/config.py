09b494e959c64180ad714abbea3ac31356fa5904
jwillmore <sbc@linuxcrusader.xyz>
2022-02-03 13:24:17 -0300

    Updated dotfiles

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -7,7 +7,8 @@ config.bind(';D', 'hint images download')
 c.url.start_pages = ["~/.config/qutebrowser/page.html"]
 c.url.default_page = "~/.config/qutebrowser/page.html"
 
-#config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.darkmode.enabled", True)
 config.set("colors.webpage.preferred_color_scheme", "dark")
 config.set("fonts.default_family", "Source Code Pro")
 config.set("content.blocking.method", "both")
+config.set("colors.webpage.darkmode.policy.images", "never")
